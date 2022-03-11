23f17646e33e0f7cc25c24a093c63e0cba48a10f
user <101434360+anton-crz@users.noreply.github.com>
2022-03-11 18:20:08 -0500

    Add files via upload

---
+++ efecto.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+   
+sleep 3
+toilet -f big '  MATRIX' -F gay | lolcat
+echo "               "[███ 10%]
+sleep 2
+echo "               "[███████ 30%]
+sleep 2
+echo "               "[██████████ 50%]
+sleep 2
+echo "               "[██████████████ 70%]
+sleep 2
+echo "               "[██████████████████ 90%]
+sleep 2
+echo "               "[██████████████████████ 100%]
+sleep 2
+echo "conectando con el servidor......"
+sleep 5
+echo ""
+echo ""
+pkg install cmatrix
+echo " "
+echo " "
+echo "instalado coreectamente"
+cmatrix
